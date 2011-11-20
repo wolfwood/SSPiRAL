5f00ba444d0c4cccaa775454303c447d1945063e
wolfwood <wolfwood@xomb.org>
2011-11-19 23:52:35 -0500

    skeleton of a working simulator, doesn't test liveness

--- markovulator.d
+++ markovulator.d
@@ -11,11 +11,48 @@ int main(char[][] argv){
 		return 1;
 	}
 
+	// --- initialization --
 	Node.setDataSize(to!(uint)(argv[1]));
+	MetaLayout deth = new MetaLayout(true);
+	MetaLayout[] curr;
+	curr = [new MetaLayout(false, Layout.startingSet())];
+
+	Stdout("digraph markovmodel {").newline;
 
 
+	// --- work loop ---
+	for(Counter layoutSize = 1; layoutSize <= Node.maxNode; layoutSize++){
+		Layout[LayoutName] next;
+
+		foreach(MetaLayout markov; curr){
+			 markov.generateLayouts(next);
+		}
+
+		MetaLayout[Score] mapping;
+
+		// generate score to markov mapping
+		foreach(Layout l; next){
+			MetaLayout* markov = l.score in mapping;
+
+			if(markov !is null){
+				markov.layouts ~= l;
+			}else{
+				mapping[l.score] = new MetaLayout(false, [l]);
+
+				//XXX:  alive, etc.
+			}
+			//l = null;
+		}
+
+		// print markov nodes
 
 
+		curr = mapping.values;
+	}
+
+
+	// footer
+	Stdout("}").newline;
 
 	return 0;
 }
--- types.d
+++ types.d
@@ -1,21 +1,34 @@
 module types;
 
-typedef uint NodeName;
+typedef ulong NodeName;
 typedef ulong LayoutName;
+alias ulong Counter;
+
+import tango.io.Stdout;
 
 struct Node{
 	static:
 	// required before use
 	void setDataSize(ulong n){
 		N = cast(NodeName)n;
-		maxNode = (cast(NodeName)2^N)-cast(NodeName)1;
+		maxNode = (cast(NodeName)1 << n)-cast(NodeName)1;
 	}
 
 	NodeName N, maxNode;
 
 	static int opApply(int delegate(ref NodeName) dg){
 		int result = 0;
-		for(NodeName i = 1; i <= maxNode; i++){
+		for(NodeName i = cast(NodeName)1; i <= maxNode; i++){
+			result = dg(i);
+			if(result)break;
+		}
+
+		return result;
+	}
+
+	static int opApply(int delegate(ref LayoutName) dg){
+		int result = 0;
+		for(LayoutName i = cast(LayoutName)1; i < (1UL << maxNode); i <<= 1){
 			result = dg(i);
 			if(result)break;
 		}
@@ -24,22 +37,138 @@ struct Node{
 	}
 }
 
-struct Layout{
+class Layout{
 	LayoutName name;
+	Score score;
+
+	this(){
+		score = new Score;
+	}
+
+	static Layout[] startingSet(){
+		Layout[] layouts;
+
+		foreach(Layout l; Layout){
+			layouts ~= l;
+		}
+
+		return layouts;
+	}
+
+	void generateLayouts(ref Layout[LayoutName] nextLayouts){
+		foreach(LayoutName node ; Node){
+			LayoutName nym = name | node;
+
+			if(nym != name){
+				Layout* l = nym in nextLayouts;
+				if(l !is null){
+					nextLayouts[nym].score += score;
+				}else{
+					Layout ell = new Layout;
+					ell.name = nym;
+					ell.score += score;
+
+					//XXX:
+					//if(alive)
+					//l is still alive
+					//else
+					// run isAlive
+
+					nextLayouts[nym] = ell;
+				}
+			}
+		}
+	}
+
+private:
+	/+int opApply(int delegate(ref Layout) dg){
+		int result = 0;
+
+		foreach(NodeName nym ; Node){
+			LayoutName next = name | cast(LayoutName)nym;
+
+			if(next != name){
+				Layout* l = new Layout;
+
+				l.name = next;
+
+				result = dg(*l);
+				if(result)break;
+			}
+		}
+
+		return result;
+	}+/
+
 
 	static int opApply(int delegate(ref Layout) dg){
 		int result = 0;
+
+		foreach(LayoutName nym ; Node){
+			Layout l = new Layout;
+			l.name = nym;
+			l.score = new Score;
+			l.score.score.length = 1;
+
+			result = dg(l);
+			if(result)break;
+		}
 
 		return result;
 	}
 }
 
+// markov mode state
 class MetaLayout{
 	Layout[] layouts;
 
-	uint name;
+	Counter name;
+	bool alive;
+	Counter coalescedNodes;
+
+	synchronized this(bool dethNode, Layout[] ls = null){
+		if(dethNode){
+			coalescedNodes++;
+		}else{
+			layouts = ls;
+			coalescedNodes += ls.length;
+		}
+
+		name = count;
+		count++;
+	}
+
+	void generateLayouts(ref Layout[LayoutName] next){
+		foreach(Layout l; layouts){
+			l.generateLayouts(next);
+			//l = null;
+		}
+	}
+
+private:
+	static Counter count;
 }
 
 class Score{
-	uint score[];
+	Score opPostInc(){
+		score[$]++;
+		return this;
+	}
+
+	Score opAddAssign(Score other){
+		if(score is null){
+			score = other.score.dup;
+		}else{
+			assert(other.score.length <= score.length);
+			for(Counter i = 0; i < other.score.length; i++){
+				score[i] += other.score[i];
+			}
+
+		}
+
+		return this;
+	}
+
+private:
+	Counter score[];
 }
