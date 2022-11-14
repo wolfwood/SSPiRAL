f0d34f069aa680ef97e771afb4dbebc925e03f0a
wolfwood <wolfwood@xomb.org>
2022-11-14 16:28:13 -0600

    Nodes and Layouts are now structs so they can have traits

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,17 +1,20 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
-use std::collections::HashMap;
 use bitintr::Blsi;
-//use std::convert::From;
+use std::collections::HashMap;
 
-type Node = u8;
-type Layout = u32;
+type NodeData = u8;
+#[derive(Clone, Copy, PartialEq, Debug, Default)]
+struct Node(NodeData);
+type LayoutData = u32;
+#[derive(Clone, Copy, PartialEq, PartialOrd, Debug, Default)]
+struct Layout(LayoutData);
 type Score = u32;
 type MlIdx = u8;
 
-const N: Node = 5;
-const M: Node = 31; //2_u8.pow(N.into())  - 1;
+const N: NodeData = 5;
+const M: NodeData = 31; //2_u8.pow(N.into())  - 1;
 const SCORE_SIZE: Score = (((M as Score) + 1) / 2) - (N as Score);
 const ML_SIZE: Score = 131 + 1;
 
@@ -49,56 +52,68 @@ impl Coeffs {
     }
 }
 
-/*impl From<Node> for Layout {
+impl From<Node> for Layout {
     fn from(n: Node) -> Self {
         //let one = 1 as Layout;
         //one << (n -1)
-	node2layout(n)
+        node2layout(n)
     }
-}*/
+}
+
+impl From<Layout> for Node {
+    fn from(l: Layout) -> Self {
+        layout2node(l)
+    }
+}
 
 // node(s) <-> layout conversion and utility functions
 fn node2layout(n: Node) -> Layout {
-    (1 as Layout) << (n - 1)
+    let Node(n) = n;
+    Layout((1 as LayoutData) << (n - 1))
 }
 
 fn layout2node(l: Layout) -> Node {
+    let Layout(l) = l;
     assert_eq!(l.count_ones(), 1);
-    l.trailing_zeros() as Node  + 1
+    Node(l.trailing_zeros() as NodeData + 1)
 }
 
-fn nodes2layout<const LIMIT: usize>(name : &[Node; LIMIT]) -> Layout {
-    let mut result = 0 as Layout;
+fn nodes2layout<const LIMIT: usize>(name: &[Node; LIMIT]) -> Layout {
+    compose_layout(name)
+}
+
+fn compose_layout<const LIMIT: usize, T: Copy>(name: &[T; LIMIT]) -> Layout
+where
+    Layout: From<T>,
+{
+    let mut result = 0 as LayoutData;
 
     for i in 0..LIMIT {
-	result |= node2layout(name[i]);
+        result |= Layout::from(name[i]).0;
     }
 
-    result
+    Layout(result)
 }
 
-fn compose_layout<const LIMIT: usize, T> /*PREDICATE: fn(T) -> Layout*/(name : &[T; LIMIT], predicate: fn(&T) -> Layout) -> Layout {
-//fn compose_layout<const LIMIT: usize, T,  PREDICATE: fn(T) -> Layout> (name : &[T; LIMIT]) -> Layout {
-   let mut result = 0 as Layout;
-
-    for i in 0..LIMIT {
-	result |= predicate(&name[i]);
-    }
-
-    result
+fn layout2nodes<const LIMIT: usize>(name: Layout) -> [Node; LIMIT] {
+    decompose_layout(name)
 }
 
-fn layout2nodes<const LIMIT: usize>(name : Layout) -> [Node; LIMIT] {
-    let mut result: [Node; LIMIT] = [0; LIMIT];
+fn decompose_layout<const LIMIT: usize, T: Default + Copy>(name: Layout) -> [T; LIMIT]
+where
+    T: From<Layout>,
+{
+    let mut result: [T; LIMIT] = [Default::default(); LIMIT];
 
-    let mut name = name;
+    let Layout(mut name) = name;
 
-    for i in (LIMIT-1)..=0 {
-	let l = smallest_in_layout(name);
-	assert_ne!(name, 0);
-	name ^= l;
+    for i in (0..LIMIT).rev() {
+        let Layout(l) = smallest_in_layout(Layout(name));
+        assert_ne!(name, 0);
+        assert_ne!(name & l, 0);
+        name ^= l;
 
-	result[i] = layout2node(l);
+        result[i] = T::from(Layout(l));
     }
 
     assert_eq!(name, 0);
@@ -107,24 +122,59 @@ fn layout2nodes<const LIMIT: usize>(name : Layout) -> [Node; LIMIT] {
 }
 
 fn largest_in_layout(l: Layout) -> Layout {
-    assert_ne!(l, 0);
+    assert_ne!(l, Layout(0));
+    let Layout(l) = l;
 
-    let result = (1 as Layout) << (Layout::BITS - 1 - l.leading_zeros());
+    let result = Layout((1 as LayoutData) << (LayoutData::BITS - 1 - l.leading_zeros()));
 
-    assert_ne!(result, 0);
+    assert_ne!(result, Layout(0));
 
     result
 }
 
 fn smallest_in_layout(l: Layout) -> Layout {
-    l.blsi()
+    let Layout(l) = l;
+    Layout(l.blsi())
 }
 
 
+//liveness
+
+fn check_if_alive(name: Layout) -> bool {
+    fn recurse_check(name: Layout, n: Node, i: Node, depth: NodeData) -> bool{
+	let Node(i) = i;
+
+	for i in (1..=i).rev(){
+	    if Layout::from(Node(i)).0 & name.0  != 0 {
+		let temp = Node(i ^ n.0);
+
+		if temp == Node(0) || (depth > 0 && recurse_check(name, temp, Node(i-1), depth - 1)){
+		    return true;
+		}
+	    }
+	}
+
+	false
+    }
+
+    for n in (1..=((1 as NodeData) << (N -1))).rev() {
+	let n = Node(n);
+	let l = Layout::from(n);
+
+	if l.0 & name.0 != 0 {
+	    if !recurse_check(name, n, Node(M), M) {
+		return false;
+	    }
+	}
+
+    }
+
+    true
+}
 
 // data structures
-struct Lay {
-    name: Layout,
+struct LayoutStats {
+    //name: Layout,
     s_idx: MlIdx,
 }
 
@@ -141,11 +191,10 @@ struct WorkContext {
     lookup: HashMap<MetaLayout, MlIdx>,
     limit: Node,
     pos: Layout,
-    curr: [Layout],
+    curr: [LayoutStats],
 }
 
 fn main() {}
-
 
 #[cfg(test)]
 mod tests {
@@ -153,57 +202,57 @@ mod tests {
 
     #[test]
     fn node2layout_conversion() {
-	assert!(N >= 2);
+        assert!(N >= 2);
 
-	assert_eq!(node2layout(1), 1);
-	assert_eq!(node2layout(2), 2);
-	assert_eq!(node2layout(3), 4);
+        assert_eq!(node2layout(Node(1)), Layout(1));
+        assert_eq!(node2layout(Node(2)), Layout(2));
+        assert_eq!(node2layout(Node(3)), Layout(4));
 
-	if N > 2 {
-	    assert_eq!(node2layout(4), 8);
-	    assert_eq!(node2layout(7), 64);
-	}
+        if N > 2 {
+            assert_eq!(node2layout(Node(4)), Layout(8));
+            assert_eq!(node2layout(Node(7)), Layout(64));
+        }
     }
 
     #[test]
     fn layout2node_conversion() {
-	assert_eq!(layout2node(2), 2);
-	assert_eq!(layout2node(16), 5);
+        assert_eq!(layout2node(Layout(2)), Node(2));
+        assert_eq!(layout2node(Layout(16)), Node(5));
     }
 
     #[test]
     fn layout_node_conversion_symmetry() {
-	{
-	    for i in M..=1 {
-		assert_eq!(layout2node(node2layout(i)), i);
-	    }
-	}
-	{
-	    let mut i = node2layout(M);
+        {
+            for i in M..=1 {
+                assert_eq!(layout2node(node2layout(Node(i))), Node(i));
+            }
+        }
+        {
+            let mut i = node2layout(Node(M)).0;
 
-	    while i > 0 {
-		assert_eq!(node2layout(layout2node(i)), i);
-		i >>= 1;
-	    }
-	}
+            while i > 0 {
+                assert_eq!(node2layout(layout2node(Layout(i))), Layout(i));
+                i >>= 1;
+            }
+        }
     }
 
     #[test]
     fn largest_smallest_in_layout() {
-	assert_eq!(smallest_in_layout(2), 2);
-	assert_eq!(largest_in_layout(2), 2);
+        assert_eq!(smallest_in_layout(Layout(2)), Layout(2));
+        assert_eq!(largest_in_layout(Layout(2)), Layout(2));
 
-	assert_eq!(largest_in_layout(5), node2layout(3));
-	assert_eq!(smallest_in_layout(5), 1);
-
+        assert_eq!(largest_in_layout(Layout(5)), node2layout(Node(3)));
+        assert_eq!(smallest_in_layout(Layout(5)), node2layout(Node(1)));
     }
 
     #[test]
     fn nodes_layout_bidirectional_conversion() {
-	const NODES1: [Node; 3] = [3,2,1];
-	const LIMIT1: usize = NODES1.len();
-	let layout1 = 7 as Layout;
+        const NODES1: [Node; 3] = [Node(3), Node(2), Node(1)];
+        const LIMIT1: usize = NODES1.len();
+        let layout1 = Layout(7);
 
-	assert_eq!(nodes2layout::<LIMIT1>(&NODES1), layout1);
+        assert_eq!(nodes2layout(&NODES1), layout1);
+        assert_eq!(layout2nodes(layout1), NODES1);
     }
 }
