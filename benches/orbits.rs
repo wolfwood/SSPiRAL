7844e6f2edc9387106720a8e62b22902080ccefd
wolfwood <transmethyl@gmail.com>
2024-08-18 15:31:22 -0700

    add a benchmark for nauty orbit computation

---
+++ benches/orbits.rs
@@ -0,0 +1,11 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use nautust::*;
+
+fn single_benchmark(c: &mut Criterion) {
+    let mut nau = Nauty::new();
+
+    c.bench_function("compute orbits", |b| b.iter(|| nau.compute()));
+}
+
+criterion_group!(benches, single_benchmark,);
+criterion_main!(benches);
